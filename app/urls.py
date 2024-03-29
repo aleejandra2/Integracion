e6555735d996d52a9b3ba3c500a7e3a5eeb3bcc3
Aleejandra <mi.currihual@duocuc.cl>
2024-03-28 22:52:18 -0300

    arreglo

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .routes import viewsAdmin
 
 urlpatterns = [
     path('', views.home, name='home'),
+    # path('administrador/contacto', viewsAdmin.index, name='contacto'),
     
     ]
